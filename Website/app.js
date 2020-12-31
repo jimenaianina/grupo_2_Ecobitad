f1e94af75e0e9cb42a9c8903283a3ec79e5f07b0
Clara Maria Diaz <claramariadiaz@MacBook-Pro-de-Clara.local>
2020-12-31 13:58:48 -0300

    Website creation

---
+++ Website/app.js
@@ -0,0 +1,19 @@
+//guardar express
+const express= require("express");
+const app= express();
+
+
+//levanto el servidor
+app.listen(3030,()=> console.log("inicio del servidor"));
+
+//configuro la carpeta public
+const path=require("path");
+
+app.use(express.static(path.resolve(__direname,"public")));
+
+//configuro las rutas para las vistas
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views",index.html)));
+app.get("/producto",(req,res)=> res.sendFile(path.resolve(__dirname,"views",product.html)));
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views",login.html)));
+app.get("/registro",(req,res)=> res.sendFile(path.resolve(__dirname,"views",register.html)));
+app.get("/carrito",(req,res)=> res.sendFile(path.resolve(__dirname,"views",cart.html)));
