9e0b6335676ec5f71e121691f1f3ecccbf5e9190
jimenaianina <jime.ianina@gmail.com>
2020-12-31 14:27:46 -0300

    agregué código

--- Website/app.js
+++ Website/app.js
@@ -9,11 +9,11 @@ app.listen(3030,()=> console.log("inicio del servidor"));
 //configuro la carpeta public
 const path=require("path");
 
-app.use(express.static(path.resolve(__direname,"public")));
+app.use(express.static(path.resolve(__dirname,"public")));
 
 //configuro las rutas para las vistas
-app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views",index.html)));
-app.get("/producto",(req,res)=> res.sendFile(path.resolve(__dirname,"views",product.html)));
-app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views",login.html)));
-app.get("/registro",(req,res)=> res.sendFile(path.resolve(__dirname,"views",register.html)));
-app.get("/carrito",(req,res)=> res.sendFile(path.resolve(__dirname,"views",cart.html)));
+app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","index.html")));
+app.get("/producto",(req,res)=> res.sendFile(path.resolve(__dirname,"views","product.html")));
+app.get("/login",(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.html")));
+app.get("/registro",(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")));
+app.get("/carrito",(req,res)=> res.sendFile(path.resolve(__dirname,"views","cart.html")));
