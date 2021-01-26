3ef558d10b9652d3c2559c35fa49f777b4cd38a4
jimenaianina <jime.ianina@gmail.com>
2021-01-26 20:39:46 -0300

    .

--- Website/app.js
+++ Website/app.js
@@ -1,15 +1,17 @@
 //guardar express
 const express= require("express");
 const app= express();
+const path = require("path");
 
 
 //levanto el servidor
 app.listen(3030,()=> console.log("inicio del servidor"));
 
 //configuro la carpeta public
-const path=require("path");
 
 app.use(express.static(path.resolve(__dirname,"public")));
+app.set('view engine', 'ejs');
+
 
 //configuro las rutas para las vistas
 app.get("/",(req,res)=> res.sendFile(path.resolve(__dirname,"views","home.html")));
