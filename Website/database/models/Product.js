ea33981b4a26e6a93ee351f49c5b4bdf6ba9bb70
jimenaianina <jime.ianina@gmail.com>
2021-04-11 18:05:52 -0300

    .

--- Website/database/models/Product.js
+++ Website/database/models/Product.js
@@ -63,7 +63,7 @@ module.exports = (sequelize, datatype)=> {
                 foreignKey: "color_id"
             },
             Product.hasMany(models.ProductImage), {
-                as:"color",
+                as:"image",
                 foreignKey: "product_image"
             }
         }
