2dee4522ce75db34c0b0a27d7fa30c19dcaa6134
jimenaianina <jime.ianina@gmail.com>
2021-04-17 13:10:48 -0300

    .

--- Website/controllers/mainController.js
+++ Website/controllers/mainController.js
@@ -5,7 +5,10 @@ const controller = {
 	index: (req,res)=> {
 	let products = JSON.parse(fs.readFileSync(path.resolve(__dirname,"../database/products.json")));	
 	return res.render("home", { title: "Home", css: "/css/home.css", products })
-	}
+	},
+	usInfo: (req,res)=> {
+		return res.render("usInfo", { title: "Nosotros", css: "/css/us.css" })
+		}
 }
 
 module.exports = controller;
