8d528a30ee7a9bbd5d9dfbe21e284fba85ff7623
jimenaianina <jime.ianina@gmail.com>
2021-04-17 20:11:13 -0300

    .

--- Website/database/config/config.js
+++ Website/database/config/config.js
@@ -8,7 +8,7 @@ module.exports= {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "Clarita10",
     "database": "integrador",
     "host": "127.0.0.1",
     "dialect": "mysql"
