cf1e749079c591878978b26ee7671fba13411ff9
jimenaianina <jime.ianina@gmail.com>
2021-04-23 16:02:19 -0300

    .

--- Website/middlewares/validateRegisterMiddleware.js
+++ Website/middlewares/validateRegisterMiddleware.js
@@ -2,11 +2,22 @@ const path = require('path');
 const { body } = require('express-validator');
 
 const validaciones = [
-    body('name').notEmpty().withMessage('Por favor escribir un nombre'), 
+    body('name').notEmpty().isLength({ min: 5 }).withMessage('Por favor escribir un nombre'), 
     body('email')
     .notEmpty().withMessage('Por favor escribir un correo electrónico')
-    .isEmail().withMessage('Por favor escribir un formato de correo válido'), 
-    body('password').notEmpty().withMessage('Por favor escribir una contraseña'), 
+    .isEmail().withMessage('Por favor escribir un formato de correo válido')
+    .custom(value => {
+        return User.findOne(value).then(user => {
+          if (user) {
+            return Promise.reject('Este correo electrónico ya se encuentra registrado');
+          }
+        }),
+    body('password')
+    .notEmpty().withMessage('Por favor escribir una contraseña')
+    .isLength({ min: 5 }).withMessage('La contraseña debe tener al menos 8 caracteres')
+    .matches('[0-9]').withMessage('Password Must Contain a Number')
+    .matches('[A-Z]').withMessage('Password Must Contain an Uppercase Letter')
+    .matches('[a-z]').withMessage('Password Must Contain a Lowercase Letter'),
     body('lastName').notEmpty().withMessage('Por favor escribir un apellido'), 
     body('image').custom((value, {req}) => {
         let file = req.file;
