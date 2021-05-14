97d0c078380e6168e5bf99bbce3b2d893875171f
Juan Pablo Paillet <pailletjuanpablo@gmail.com>
2021-05-14 01:49:43 -0300

    Correccion en validacion de usuario asincrona

--- Website/middlewares/validateRegisterMiddleware.js
+++ Website/middlewares/validateRegisterMiddleware.js
@@ -4,22 +4,27 @@ const db = require('../database/models');
 const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,20}$/;
 
 const validacionRegistro = [
-    body('name').notEmpty().isLength({ min: 2 }).withMessage('El nombre debe tener al menos 2 caracteres'), 
-    body('lastName').notEmpty().isLength({ min: 2 }).withMessage('El apellido debe tener al menos 2 caracteres'), 
+    body('name').notEmpty().isLength({ min: 2 }).withMessage('El nombre debe tener al menos 2 caracteres'),
+    body('lastName').notEmpty().isLength({ min: 2 }).withMessage('El apellido debe tener al menos 2 caracteres'),
     body('email')
-    .notEmpty()
-    .isEmail()
-    .custom(value => { db.User.findOne(value)
-        .then(user => {
-          if (user) {
-            return Promise.reject('Este correo electrónico ya se encuentra registrado');
-          }})
+        .notEmpty()
+        .isEmail()
+        .custom(async value => {
+            // El validador funcionará de forma asíncrona
+            // En findOne hay que pasarle la condición where, no el string directo
+            const existentUser = await db.User.findOne({ where: { email: value } });
+            // Si existent user encontró un valor, dispara error, caso contrario retorna true
+            if (existentUser) {
+                throw new Error('Este correo electrónico ya se encuentra registrado');
+            }
+            return true;
+
         }),
     body('password')
-    .notEmpty()
-    .isLength({ min: 8 })
-    .matches(regex),
-    body('image').custom((value, {req}) => {
+        .notEmpty()
+        .isLength({ min: 8 }),
+    //   .matches(regex),
+    body('image').custom((value, { req }) => {
         let file = req.file;
         let acceptedExtensions = ['.jpg', '.png', '.gif'];
         if (!file) {
@@ -27,11 +32,11 @@ const validacionRegistro = [
         } else {
             let fileExtension = path.extname(file.originalname);
             if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error ("Las extensiones del archivo permitidas son " + acceptedExtensions.join(', '));
+                throw new Error("Las extensiones del archivo permitidas son " + acceptedExtensions.join(', '));
             }
         }
         return true;
-        })
-    ];
+    })
+];
 
 module.exports = validacionRegistro
