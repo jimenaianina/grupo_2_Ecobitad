b9cde6bf75708203a1b3cd8a32da2ba0228973b6
jimenaianina <jime.ianina@gmail.com>
2021-05-20 07:18:16 -0300

    .

--- Website/controllers/cartController.js
+++ Website/controllers/cartController.js
@@ -1,29 +1,28 @@
 const db = require('../database/models');
+const { forEach } = require('../middlewares/validateRegisterMiddleware');
 
 const controller = {
 
     cart: async (req,res) => {
-    
-        //este controller rompe todo, pero lo ideal sería que traiga el cartUser y el cartProducts del cartUser 
-        //y pasárselos a la vista para que muestre los productos en el cart
         
         let userLogged = req.session.userLogged
         
         if(userLogged) {
-    /*
+    
         let cart = await db.Cart.findOne({
             where: { 
                 user_id: userLogged.id
-            }
+            },
+            attributes: {exclude: ['UserId']}
         });
     
-        let cartProducts = await db.CartProduct.findOne({ 
+        let cartProducts = await db.CartProduct.findAll({ 
             where: {
                 cart_id: cart.id
             }
         })
-    */
-        return res.render("products/cart2", { title: "Carrito", css: "/css/cart2.css"})
+    
+        return res.render("products/cart2", { title: "Carrito", css: "/css/cart2.css", cart: cart, cartProducts: cartProducts})
     } else { return res.redirect("/usuario/acceder")}
     },
     
@@ -55,29 +54,35 @@ const controller = {
             unit_price: productToAdd.price,
         })
     
-        // lo ideal sería sumar todos los unit_price de los productsOnCart  y actualizar el cart_total de cartUser
-    
+        let arrayOfPrices = [];
+
         let productsOnCart = await db.CartProduct.findAll({ 
             where: { 
                 cart_id : cartUser.id
              }
             })
-        return res.send(productsOnCart)
-        let cartTotal = 0;
-        /*
+
+        productsOnCart.forEach( product => {
+            arrayOfPrices.push(product.unit_price)})
+        
+        let cartTotal = arrayOfPrices.reduce(function(a, b){return a + b})
+        
         cartUser.cart_total = cartTotal
-        await cartUser.save();*/
+        await cartUser.save();
     
-        return res.render("products/cart2", { title: "Carrito", css: "/css/cart2.css", colors, categories, sizes })
+        return res.redirect('/carrito', { title: "Carrito", css: "/css/cart2.css" })
     } else { return res.redirect("/usuario/acceder")}
     } catch(error) {return res.send(error)}
     },
     
     destroyCart: async (req, res) => {
             try { 
-                /*let cartsToDelete = await db.Cart.findAll({
+
+                let userLogged = req.session.userLogged;
+
+                let cartsToDelete = await db.Cart.findOne({
                     where: {
-                        user_id: 12
+                        user_id: userLogged.id
                     }
                 })
     
@@ -86,13 +91,7 @@ const controller = {
                         cart_id: cartsToDelete.id
                     }
                 });
-    
-                await cartsToDelete.destroy({
-                    where: {
-                        user_id: 12
-                    }
-                });*/
-            
+
             return res.redirect('/');
     
         } catch(error) {return res.send(error)}
